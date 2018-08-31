7eb480292d31ceaf015cc2728439775a33100cdb
Michael <micharch54@gmail.com>
2018-08-31 13:59:36 -0600

    Initial commit

---
+++ lib/status_page.rb
@@ -0,0 +1,5 @@
+require "status_page/version"
+
+module StatusPage
+  # Your code goes here...
+end
---
+++ spec/status_page_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe StatusPage do
+  it "has a version number" do
+    expect(StatusPage::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
